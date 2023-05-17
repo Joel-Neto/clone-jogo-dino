1aa7a9f385538b699cf908c6617db31d14f6d428
Joel Neto <133702725+Joel-Neto@users.noreply.github.com>
2023-05-17 12:15:27 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const personagem = document.querySelector('#personagem')
+const quadrado = document.querySelector('#quadrado')
+
+function pular(){
+  if (personagem.classList !== 'animar'){
+    personagem.classList.add('animar')
+  }
+
+  setTimeout(function(){
+    personagem.classList.remove('animar')
+  }, 500)
+} 
+
+let testarColisao = setInterval(function(){
+  let topoPersonagem = parseInt(window.getComputedStyle(personagem).getPropertyValue('top'))
+  let esquerdaQuadrado = parseInt(window.getComputedStyle(quadrado).getPropertyValue('left'))
+
+  if(esquerdaQuadrado < 20 && esquerdaQuadrado > 0 && topoPersonagem >= 130){
+    quadrado.style.animation = 'none'
+    quadrado.style.display = 'none'
+    alert('Você perdeu!')
+  }
+}, 10)
